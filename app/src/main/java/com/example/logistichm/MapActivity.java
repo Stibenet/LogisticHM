bf9593f564679a8f4358691316c1e614c613d552
Unknown <fatkullinmars23@gmail.com>
2019-11-07 16:26:45 +0500

    Добавлено левое меню, тестовое отображение заказов

--- app/src/main/java/com/example/logistichm/MapActivity.java
+++ app/src/main/java/com/example/logistichm/MapActivity.java
@@ -1,14 +1,71 @@
 package com.example.logistichm;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.logistichm.Models.Order;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MapActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
+    RecyclerView mRecyclerView;
+    FirebaseDatabase mFirebaseDatabase;
+    DatabaseReference mRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        //Кнопка меню для левого меню
+        mDrawerLayout = findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //RecyclerView
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mRef = mFirebaseDatabase.getReference("Data");
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        if (mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        FirebaseRecyclerAdapter<Order, ViewHolder> firebaseRecyclerAdapter =
+                new FirebaseRecyclerAdapter<Order, ViewHolder>(
+                        Order.class,
+                        R.layout.row,
+                        ViewHolder.class,
+                        mRef
+                ) {
+                    @Override
+                    protected void populateViewHolder(ViewHolder viewHolder, Order order, int position) {
+                        viewHolder.setDetails(getApplicationContext(), order.getTitle(), order.getDescription(), order.getImage());
+                    }
+                };
+        mRecyclerView.setAdapter(firebaseRecyclerAdapter);
     }
 }
---
+++ app/src/main/java/com/example/logistichm/Models/Order.java
@@ -0,0 +1,32 @@
+package com.example.logistichm.Models;
+
+public class Order {
+    private String title, image, description;
+
+    public Order() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
