bca815ada4fec3af98eb9cea7ba3548623874b6b
Unknown <fatkullinmars23@gmail.com>
2019-11-08 15:27:25 +0500

    Переработано левое меню, кнопки не активны

---
+++ app/src/main/java/com/example/logistichm/CreateOrder.java
@@ -0,0 +1,4 @@
+package com.example.logistichm;
+
+public class CreateOrder {
+}
--- app/src/main/java/com/example/logistichm/ListOrder.java
+++ app/src/main/java/com/example/logistichm/ListOrder.java
@@ -1,24 +1,19 @@
 package com.example.logistichm;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
 
 import com.example.logistichm.Models.Order;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MapActivity extends AppCompatActivity {
-    private DrawerLayout mDrawerLayout;
-    private ActionBarDrawerToggle mToggle;
-
+public class ListOrder extends AppCompatActivity {
     RecyclerView mRecyclerView;
     FirebaseDatabase mFirebaseDatabase;
     DatabaseReference mRef;
@@ -26,14 +21,7 @@ public class MapActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
-
-        //Кнопка меню для левого меню
-        mDrawerLayout = findViewById(R.id.drawer);
-        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
-        mDrawerLayout.addDrawerListener(mToggle);
-        mToggle.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setContentView(R.layout.list_orders);
 
         //RecyclerView
         mRecyclerView = findViewById(R.id.recyclerView);
@@ -44,15 +32,8 @@ public class MapActivity extends AppCompatActivity {
         mRef = mFirebaseDatabase.getReference("Data");
     }
 
-    public boolean onOptionsItemSelected(MenuItem item){
-        if (mToggle.onOptionsItemSelected(item)){
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onStart(){
+        @Override
+    protected void onStart() {
         super.onStart();
         FirebaseRecyclerAdapter<Order, ViewHolder> firebaseRecyclerAdapter =
                 new FirebaseRecyclerAdapter<Order, ViewHolder>(
